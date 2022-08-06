1576c0e3ef50ca6f62a718236e2369f51c9176c0
cysersery <cyserser@gmail.com>
2022-08-06 13:00:43 +0200

    Line Tracing & Impact Effects

--- Source/SimpleShooter/Gun.cpp
+++ Source/SimpleShooter/Gun.cpp
@@ -4,6 +4,7 @@
 #include "Gun.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values
 AGun::AGun()
@@ -34,5 +35,36 @@ void AGun::Tick(float DeltaTime)
 void AGun::PullTrigger()
 {
 	UGameplayStatics::SpawnEmitterAttached(MuzzleFlash, Mesh, TEXT("MuzzleFlashSocket"));
+
+	APawn* OwnerPawn = Cast<APawn>(GetOwner());
+	if(OwnerPawn == nullptr)
+	{
+		return;
+	}
+
+	AController* OwnerController = OwnerPawn->GetController();
+	if(OwnerController == nullptr)
+	{
+		return;
+	}
+
+	FVector Location;
+	FRotator Rotation;
+	OwnerController->GetPlayerViewPoint(Location, Rotation);
+
+	FVector End = Location + Rotation.Vector() * MaxRange;
+
+	FHitResult Outhit;
+	bool IsHit = GetWorld()->LineTraceSingleByChannel(
+		Outhit,
+		Location, 
+		End, 
+		ECollisionChannel::ECC_GameTraceChannel1);
+	
+	if(IsHit)
+	{
+		FVector ShotDirection = -Rotation.Vector();
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), HitEffect, Outhit.Location, ShotDirection.Rotation());	
+	}
 }
 
