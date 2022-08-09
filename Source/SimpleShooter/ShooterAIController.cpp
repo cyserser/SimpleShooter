8acff144fdc472d3932cc293cfa43d4aa93b81bd
cysersery <cyserser@gmail.com>
2022-08-09 13:45:43 +0200

    Creating Nav Mesh & Adding Line of Sight AI

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -3,14 +3,38 @@
 
 #include "ShooterAIController.h"
 #include "Kismet/GameplayStatics.h"
+#include "ShooterCharacter.h"
 
 // Called when the game starts or when spawned
 void AShooterAIController::BeginPlay()
 {
 	Super::BeginPlay();
+    
+    if(AIBehavior != nullptr)
+    {
+        RunBehaviorTree(AIBehavior);
+    }
+}
+
+// Called every frame
+void AShooterAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
 
     APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 
-    SetFocus(PlayerPawn);
+    
 
+    // if(LineOfSightTo(PlayerPawn))
+    // {
+    //     SetFocus(PlayerPawn);
+    //     MoveToActor(PlayerPawn, AcceptanceRadius);
+    // }
+    // else 
+    // {
+    //    ClearFocus(EAIFocusPriority::Gameplay);
+    //    StopMovement();
+    // }
+    
+    
 }
--- Source/SimpleShooter/ShooterCharacter.cpp
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -9,7 +9,6 @@ AShooterCharacter::AShooterCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
@@ -30,7 +29,6 @@ void AShooterCharacter::BeginPlay()
 void AShooterCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
